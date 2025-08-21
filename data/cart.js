b627cba3d7e13458cbf5edd3a21cb070837a27f3
Sydney <Sydneychristopherjnr@gmail.com>
2025-08-21 21:55:16 +0100

    create oop version of cart

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,9 @@
-export let cart =
-    JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
         cart =    [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
@@ -11,6 +15,8 @@ if (!cart) {
     deliveryOptionId: '2'
 }]; 
 };
+}
+   
  
 
 export function addToCart(productId) {
